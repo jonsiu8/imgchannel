0b31a8040115dfcbb1bfe84ba29fdc53108c1bba
jon <jonsiu@gmail.com>
2019-10-30 18:51:34 +0800

    career page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.facebook.com/imgch/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 {/*<li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
