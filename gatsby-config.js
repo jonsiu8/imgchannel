ffda85c7bd0193cc97bc785628564ba94199babe
jon <jonsiu@gmail.com>
2019-10-31 09:59:38 +0800

    progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,15 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     'gatsby-plugin-react-helmet',
