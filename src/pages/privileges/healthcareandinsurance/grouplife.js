2e0ad5af75b0cd8d2879b3c6f306867e180a181b
jon <jonsiu@gmail.com>
2019-11-12 19:05:29 +0800

    group life page

--- src/pages/privileges/healthcareandinsurance/grouplife.js
+++ src/pages/privileges/healthcareandinsurance/grouplife.js
@@ -1,82 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../../../components/layout'
-import styled from "styled-components"
+import { Link } from 'gatsby'
 import { graphql } from "gatsby"
 import Img from 'gatsby-image'
-
-const ContainerHealth = styled.div`  
-    background-color:white;  
-    h4{color:#0B258F};  
-    h3{color:#0B258F};
-    h2{color:#0B258F};
-    h1{color:#0B258F};  
-    color: black;
-    b{color:black};
-    th{color:black};
-    tr{border:solid 1px black};
-
-    .button{
-        background-color:#3277B3;
-        color:white;     
-    }
-    hr.line1 {border-top: 1px solid black;}    
-    box-shadow: 5px 5px 5px #1E1E1E;
-    padding: 30px;
-    margin: 3rem auto;
-    max-width: 1200px;
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-    justify-content: left;
-    `
-
-    const ContainerForm = styled.div`  
-    background-color:#0B258F;
-    h4{color:white};  
-    h3{color:white};
-    h2{color:white};
-    color: white;
-    input:invalid {
-        border: 1px solid red;
-      }
-      
-      input:valid {
-        border: 1px solid green;
-      }
-
-      textarea:invalid {
-        border: 1px solid red;
-      }
-      
-      textarea:valid {
-        border: 1px solid green;
-      }
-
-    .button{
-        background-color:#0B258F;
-        color:white;     
-    }
-    hr.line1 {border-top: 1px solid black;}    
-    box-shadow: 5px 5px 5px #1E1E1E;
-    padding: 25px;
-    margin: 1px;   
-    `
-
-    const Ulist = styled.ul`  
-    list-style-type: none;
-    color:black;
-    font-size: 18px;
-    p{  font-size: 20px;
-        padding-bottom:20px;
-        margin:0px
-    };
-    b{color:black};
-
-    @media (max-width: 768px) {
-        font-size: 15px;
-      }
-  `
     
 const Grouplife = (props) => (
     <Layout>
@@ -92,19 +19,29 @@ const Grouplife = (props) => (
                     </header>
                     <div className="grid-wrapper">
                         <div className="col-4">
-                            <ContainerHealth> 
-                                <span className="image fit"><Img fluid={props.data.image02.childImageSharp.fluid} /></span>
-                            </ContainerHealth>
+                            <span className="image fit"><Img fluid={props.data.image01.childImageSharp.fluid} /></span>
+                        </div>                        
+                        <div className="col-7">                          
+                            <h2>P50,000 Life Insurance* for all IMG Members</h2>
+                            <h3>with 24/7 memorial concierge service tie-up with Everest</h3>
+                            <p>* Need to fll out application form subject to underwriting process<br/>
+                                * Pay one-time membership fee of P500 for Fidelity</p>
+                            <hr className="major"/>                    
                         </div>
-                        
-                        <div className="col-8">
-                            <ContainerHealth>
-                                <h2>100,000 Accident Insurance for all IMG Members</h2>
-                            </ContainerHealth>
+    
+                        <div className="col-4">
+                            <span className="image fit"><Img fluid={props.data.image02.childImageSharp.fluid} /></span>
+                        </div>                        
+                        <div className="col-7">                          
+                            <h2>P100,000 Personal Accident Insurance* for all IMG Members</h2>                            
+                            <p>* Standard underwriting guidelines will apply</p>
+                            <hr className="major"/>                       
                         </div>
-                        
+                        <div className="col-12">
+                            <h4>Contact us for more information about group life insurance.</h4>
+                            <Link to="../../contact" className="button small">Contact</Link>
+                        </div>                                            
                     </div> 
-            
                 </div>
             </section>
         </div>
@@ -125,7 +62,7 @@ fragment fluidImage on File {
 
 export const pageQuery = graphql`
   query {
-    image01: file(relativePath: { eq: "kaiser-card.jpg" }) {
+    image01: file(relativePath: { eq: "fidelitylife-banner.png" }) {
       ...fluidImage
     }
     image02: file(relativePath: { eq: "mblife-banner.png" }) {
