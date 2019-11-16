f8e309074675bf5170592db5ca37b7199b9410dc
jon <jonsiu@gmail.com>
2019-11-16 15:40:16 +0800

    og image affiliates

--- src/pages/affiliates.js
+++ src/pages/affiliates.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 import BannerAffiliates from '../components/BannerAffiliates'
 import { graphql } from "gatsby"
 import Img from 'gatsby-image'
-import previewpic from '../assets/images/ogimage-privileges.png'
+import previewpic from '../assets/images/ogimage-affiliates.png'
 
 const Container = styled.div`  
     background-color: white;  
