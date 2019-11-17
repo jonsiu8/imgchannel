5da6c5184a5fc5050c5da0e22304484bf501417e
jon <jonsiu@gmail.com>
2019-11-17 09:24:57 +0800

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,45 +1,60 @@
 import React from 'react'
-import { Link, graphql , useStaticQuery } from 'gatsby'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
-const BlogPage = () => {
-    const data = useStaticQuery(graphql`
-        query  {
-            allMarkdownRemark {
-                edges {
-                    node {
-                        frontmatter {
-                            author
-                            date
-                            path
-                            title
-                        }
-                        fields {
-                            slug
-                        }
+import pic11 from '../assets/images/pic11.jpg'
+
+import { graphql, Link } from 'gatsby'
+
+const Generic = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Blog Page</title>
+            <meta name="description" content="Blog - IMG Channel" />
+        </Helmet>
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Blog Page</h1>
+                    </header>
+                    <span className="image main"><img src={pic11} alt="" /></span>
+                    <ol>
+                        {props.data.allMarkdownRemark.edges.map((edge) => {
+                            return (
+                                <li>
+                                    <Link to ={`/blog/${edge.node.fields.slug}`}>
+                                    <h3>{edge.node.frontmatter.title}</h3>
+                                    <p>{edge.node.frontmatter.date} - by:{edge.node.frontmatter.author}</p>
+                                    </Link>
+                                </li>
+                            )
+                        })}                
+                    </ol>
+                </div>
+            </section>
+        </div>
+    </Layout>
+)
+
+export default Generic
+
+export const query = graphql`
+    query  {
+        allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        author
+                        date
+                        path
+                        title
+                    }
+                    fields {
+                        slug
                     }
                 }
             }
         }
-    `)
-
-    return (
-        <div>
-            <h1>Blog Page</h1>
-            <h2>Post will be here</h2>
-            <ol>
-                {data.allMarkdownRemark.edges.map((edge) => {
-                    return (
-                        <li>
-                            <Link to ={`/blog/${edge.node.fields.slug}`}>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                            <p>{edge.node.frontmatter.date}</p>
-                            </Link>
-                        </li>
-                    )
-                })}                
-            </ol>
-        </div>
-    )
-}
-
-export default BlogPage
+    }
+    `
