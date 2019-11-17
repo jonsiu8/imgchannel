357e8c8e4f9c419bcf0d53b6298a8fdeb25469cd
jon <jonsiu@gmail.com>
2019-11-17 11:00:45 +0800

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Layout from '../components/layout'
-
-import pic11 from '../assets/images/pic11.jpg'
-
+import styled from "styled-components"
 import { graphql, Link } from 'gatsby'
+import Layout from '../components/layout'
+import BannerBlog from '../components/BannerBlog'
+
+const BlogTitleContainer = styled.ol`  
+    
+    list-style-type: none;
+    margin: 0;
+    `
+const BlogList = styled.li`
+    margin: 1rem o;
+    a{
+        background: #f4f4f4;
+        color: #000000;
+        h3{color: #000000;};
+        display: block;
+        padding: 1rem;
+        text-decoration: none;
+    }
+
+    a:hover{
+        background: #e4e4e4;
+    }
+
+    h3 {
+        margin-bottom: 0;
+    }
+
+    p {
+        color: #777777;
+        font-size: 1rem;
+        font-style: italic;
+    }
+`
+
+
 
 const Generic = (props) => (
     <Layout>
@@ -12,25 +44,22 @@ const Generic = (props) => (
             <title>Blog Page</title>
             <meta name="description" content="Blog - IMG Channel" />
         </Helmet>
+        <BannerBlog/>
         <div id="main" className="alt">
             <section id="one">
                 <div className="inner">
-                    <header className="major">
-                        <h1>Blog Page</h1>
-                    </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
-                    <ol>
+                    <BlogTitleContainer>
                         {props.data.allMarkdownRemark.edges.map((edge) => {
                             return (
-                                <li>
+                                <BlogList>
                                     <Link to ={`/blog/${edge.node.fields.slug}`}>
                                     <h3>{edge.node.frontmatter.title}</h3>
                                     <p>{edge.node.frontmatter.date} - by:{edge.node.frontmatter.author}</p>
                                     </Link>
-                                </li>
+                                </BlogList>
                             )
                         })}                
-                    </ol>
+                    </BlogTitleContainer>
                 </div>
             </section>
         </div>
