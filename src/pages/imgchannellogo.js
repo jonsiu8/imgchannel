fdb2c214c4063156f64d48ba176cb9e5b65b6db9
jon <jonsiu@gmail.com>
2019-12-03 23:07:07 +0800

    imgchannellogo

---
+++ src/pages/imgchannellogo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import imgchlogosig from '../assets/images/imgchlogosig.png'
+
+const ImgChannelLogo = () => {
+    return (
+        <span><img src={imgchlogosig} alt="" /></span>
+    )
+}
+
+export default ImgChannelLogo
