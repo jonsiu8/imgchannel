3afa03b1fa6ea5e74d76bc065c42283d84a9bb44
jon <jonsiu@gmail.com>
2020-02-16 23:50:40 +0800

    tag manager google

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,23 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-load-script",
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        src: "https://www.imgchannel.com/replainscript.js",
+        id: "6000573762",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        //dataLayerName: "YOUR_DATA_LAYER_NAME",
       },
     },
     // {
--- static/replainscript.js
+++
@@ -1,4 +0,0 @@
-window.replainSettings = { id: '0ea1f7dc-63df-4395-9afd-0c9786179563' };
-(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
-var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
-})('https://widget.replain.cc/dist/client.js');
