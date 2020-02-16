a34f5126e1f8e638541e03a7d89cc07f97dae06a
jon <jonsiu@gmail.com>
2020-02-16 23:54:07 +0800

    tag manager google

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         // Specify optional GTM environment details.
         //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
         //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        //dataLayerName: "YOUR_DATA_LAYER_NAME",
+        dataLayerName: "GTM-KZKML3X",
       },
     },
     // {
